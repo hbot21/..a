10fccae0168bb8cca077f916ec2ea242495e486c
hbot21 <43694980+hbot21@users.noreply.github.com>
2018-10-25 07:50:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,264 +1,192 @@
-const Discord = require("discord.js");
-const ytdl = require("ytdl-core");
-const { Client, Util } = require('discord.js');
-const getYoutubeID = require('get-youtube-id');
-const fetchVideoInfo = require('youtube-info');
-const YouTube = require('simple-youtube-api');
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-const queue = new Map();
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
-/*
-البكجآت
-npm install discord.js
-npm install ytdl-core
-npm install get-youtube-id
-npm install youtube-info
-npm install simple-youtube-api
-npm install queue
-*/
-
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    console.log(`in ${client.guilds.size} servers `)
-    console.log(`[Codes] ${client.users.size}`)
-    client.user.setStatus("idle")
+    console.log('I am ready!');
 });
-//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-const prefix = "A"
-client.on('message', async msg => { // eslint-disable-line
-	if (msg.author.bot) return undefined;
-	//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-	if (!msg.content.startsWith(prefix)) return undefined;
-	const args = msg.content.split(' ');
-	const searchString = args.slice(1).join(' ');
-	//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-	const url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
-	const serverQueue = queue.get(msg.guild.id);
-//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-	let command = msg.content.toLowerCase().split(" ")[0];
-	command = command.slice(prefix.length)
-//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-	if (command === `play`) {
-		const voiceChannel = msg.member.voiceChannel;
-		if (!voiceChannel) return msg.channel.send('يجب توآجد حضرتك بروم صوتي .');
-		const permissions = voiceChannel.permissionsFor(msg.client.user);
-		if (!permissions.has('CONNECT')) {
-			//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-			return msg.channel.send('لا يتوآجد لدي صلاحية للتكلم بهذآ الروم');
-		}//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-		if (!permissions.has('SPEAK')) {
-			return msg.channel.send('لا يتوآجد لدي صلاحية للتكلم بهذآ الروم');
-		}//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
 
-		if (!permissions.has('EMBED_LINKS')) {
-			return msg.channel.sendMessage("**يجب توآفر برمشن `EMBED LINKS`لدي **")
-		}
-
-		if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
-			const playlist = await youtube.getPlaylist(url);
-			const videos = await playlist.getVideos();
-			//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-			for (const video of Object.values(videos)) {
-				const video2 = await youtube.getVideoByID(video.id); // eslint-disable-line no-await-in-loop
-				await handleVideo(video2, msg, voiceChannel, true); // eslint-disable-line no-await-in-loop
-			}//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-			return msg.channel.send(` **${playlist.title}** تم الإضآفة إلى قأئمة التشغيل`);
-		} else {
-			try {//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-
-				var video = await youtube.getVideo(url);
-			} catch (error) {
-				try {//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-					var videos = await youtube.searchVideos(searchString, 5);
-					let index = 0;
-					const embed1 = new Discord.RichEmbed()
-			        .setDescription(`**الرجآء من حضرتك إختيآر رقم المقطع** :
-${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
-//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-					.setFooter(" - MaF , ` $")
-					msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
-					
-					// eslint-disable-next-line max-depth
-					try {
-						var response = await msg.channel.awaitMessages(msg2 => msg2.content > 0 && msg2.content < 11, {
-							maxMatches: 1,
-							time: 15000,
-							errors: ['time']
-						});//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-					} catch (err) {
-						console.error(err);
-						return msg.channel.send('لم يتم إختيآر مقطع صوتي');
-					}
-					const videoIndex = parseInt(response.first().content);
-					var video = await youtube.getVideoByID(videos[videoIndex - 1].id);
-				} catch (err) {
-					console.error(err);
-					return msg.channel.send(':X: لا يتوفر نتآئج بحث ');
-				}
-			}//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-
-			return handleVideo(video, msg, voiceChannel);
-		}//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-	} else if (command === `skip`) {
-		if (!msg.member.voiceChannel) return msg.channel.send('أنت لست بروم صوتي .');
-		if (!serverQueue) return msg.channel.send('لا يتوفر مقطع لتجآوزه');
-		serverQueue.connection.dispatcher.end('تم تجآوز هذآ المقطع');
-		return undefined;
-	} else if (command === `stop`) {//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-		if (!msg.member.voiceChannel) return msg.channel.send('أنت لست بروم صوتي .');
-		if (!serverQueue) return msg.channel.send('لا يتوفر مقطع لإيقآفه');
-		serverQueue.songs = [];
-		serverQueue.connection.dispatcher.end('تم إيقآف هذآ المقطع');
-		return undefined;
-	} else if (command === `vol`) {
-		if (!msg.member.voiceChannel) return msg.channel.send('أنت لست بروم صوتي .');
-		if (!serverQueue) return msg.channel.send('لا يوجد شيء شغآل.');
-		if (!args[1]) return msg.channel.send(`:loud_sound: مستوى الصوت **${serverQueue.volume}**`);
-		serverQueue.volume = args[1];//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-		serverQueue.connection.dispatcher.setVolumeLogarithmic(args[1] / 50);
-		return msg.channel.send(`:speaker: تم تغير الصوت الي **${args[1]}**`);
-	} else if (command === `np`) {
-		if (!serverQueue) return msg.channel.send('لا يوجد شيء حالي ف العمل.');
-		const embedNP = new Discord.RichEmbed()
-	.setDescription(`:notes: الان يتم تشغيل : **${serverQueue.songs[0].title}**`)
-		return msg.channel.sendEmbed(embedNP);
-	} else if (command === `queue`) {
-		//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-		if (!serverQueue) return msg.channel.send('لا يوجد شيء حالي ف العمل.');
-		let index = 0;
-		//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-		const embedqu = new Discord.RichEmbed()
-//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-.setDescription(`**Songs Queue**
-${serverQueue.songs.map(song => `**${++index} -** ${song.title}`).join('\n')}
-**الان يتم تشغيل** ${serverQueue.songs[0].title}`)
-		return msg.channel.sendEmbed(embedqu);
-	} else if (command === `pause`) {
-		if (serverQueue && serverQueue.playing) {
-			serverQueue.playing = false;
-			serverQueue.connection.dispatcher.pause();
-			return msg.channel.send('تم إيقاف الموسيقى مؤقتا!');
-		}//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-		return msg.channel.send('لا يوجد شيء حالي ف العمل.');
-	} else if (command === "resume") {
-		if (serverQueue && !serverQueue.playing) {
-			serverQueue.playing = true;
-			serverQueue.connection.dispatcher.resume();
-			return msg.channel.send('استأنفت الموسيقى بالنسبة لك !');
-		}//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-		return msg.channel.send('لا يوجد شيء حالي في العمل.');
-	}
-
-	return undefined;
-});
-//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-async function handleVideo(video, msg, voiceChannel, playlist = false) {
-	const serverQueue = queue.get(msg.guild.id);
-	console.log(video);
-	//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-//	console.log('yao: ' + Util.escapeMarkdown(video.thumbnailUrl));
-	const song = {
-		id: video.id,
-		title: Util.escapeMarkdown(video.title),
-		url: `https://www.youtube.com/watch?v=${video.id}`
-	};//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-	if (!serverQueue) {
-		const queueConstruct = {
-			textChannel: msg.channel,
-			voiceChannel: voiceChannel,
-			connection: null,
-			songs: [],
-			volume: 5,
-			playing: true
-		};//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-		queue.set(msg.guild.id, queueConstruct);
-//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-		queueConstruct.songs.push(song);
-//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-		try {
-			var connection = await voiceChannel.join();
-			queueConstruct.connection = connection;
-			play(msg.guild, queueConstruct.songs[0]);
-		} catch (error) {
-			console.error(`I could not join the voice channel: ${error}`);
-			queue.delete(msg.guild.id);
-			return msg.channel.send(`لا أستطيع دخول هذآ الروم ${error}`);
-		}
-	} else {//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-		serverQueue.songs.push(song);
-		console.log(serverQueue.songs);
-		if (playlist) return undefined;
-		else return msg.channel.send(` **${song.title}** تم اضافه الاغنية الي القائمة!`);
-	}
-	return undefined;
-}//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-
-function play(guild, song) {
-	const serverQueue = queue.get(guild.id);
-
-	if (!song) {//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-		serverQueue.voiceChannel.leave();
-		queue.delete(guild.id);
-		return;//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-	}//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-	console.log(serverQueue.songs);
-//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-	const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
-		.on('end', reason => {//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-			if (reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
-			else console.log(reason);
-			serverQueue.songs.shift();//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-			play(guild, serverQueue.songs[0]);
-		})//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-		.on('error', error => console.error(error));//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-
-	serverQueue.textChannel.send(`بدء تشغيل : **${song.title}**`);
-}//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-
-const adminprefix = "A";
-const devs = ['300306052610523136'];
 client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-   
-if (message.content.startsWith(adminprefix + 'setgame')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
-} else
-  if (message.content.startsWith(adminprefix + 'setname')) {
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
-return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
-} else
-  if (message.content.startsWith(adminprefix + 'setavatar')) {
-client.user.setAvatar(argresult);
-  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
-      } else    
-if (message.content.startsWith(adminprefix + 'setT')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/idk");
-    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
-}
+    if (message.content === 'ping') {
+        message.reply('pong');
+      }
+});
+
+
+let logs = JSON.parse(fs.readFileSync(`./logs.json`, `utf8`)); // BY ! - NourEldien.#8007
+client.on('message', message => {
+  if(!logs[message.guild.id]) logs[message.guild.id] = {
+  onoff: 'Off',
+  channel: 'logs' // BY ! - NourEldien.#8007
+  };
+  if(logs[message.guild.id].onoff === 'Off') return;
+  let logchannel = message.guild.channels.find("name", logs[message.guild.id].channel)
  
 });
-
-client.on("message", message => {
- if (message.content === `${prefix}help`) {
-  const embed = new Discord.RichEmbed() //by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-      .setColor("#000000")//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-      .setDescription(`
-${prefix}join ? دخول البوت الي الروم
-${prefix}play ? لتشغيل أغنية برآبط أو بأسم
-${prefix}skip ? لتجآوز الأغنية الحآلية
-${prefix}pause ? إيقآف الأغنية مؤقتا
-${prefix}resume ? لموآصلة الإغنية بعد إيقآفهآ مؤقتا
-${prefix}vol ? لتغيير درجة الصوت 100 - 0
-${prefix}stop ? لإخرآج البوت من الروم
- `)//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-   message.channel.sendEmbed(embed)//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-    
-   }
-   }); 
+client.on('message', message => {
+  client.on("roleCreate", rc => {
+    const channel = rc.guild.channels.find("name", logs[message.guild.id].channel)
+    if(channel) {
+    var embed = new Discord.RichEmbed()
+    .setTitle(rc.guild.name) // BY ! - NourEldien.#8007
+    .setDescription(`***Created Role Name : *** **${rc.name}** `)
+    .setColor(`RANDOM`)
+    .setTimestamp(); // BY ! - NourEldien.#8007
+    channel.sendEmbed(embed)
+    }
+    });
+    //By S Codes
+    client.on("roleDelete",  rd => {
+    const channel = rd.guild.channels.find("name", logs[message.guild.id].channel)
+    if(channel) {
+    var embed = new Discord.RichEmbed()
+    .setTitle(rd.guild.name)
+    .setDescription(`***Deleted Role Name : *** **${rd.name}** `)
+    .setColor(`RANDOM`)
+    .setTimestamp(); // BY ! - NourEldien.#8007
+    channel.sendEmbed(embed)
+    }
+    });
+    // BY ! - NourEldien.#8007
+  client.on("channelCreate",  cc => {
+    const channel = cc.guild.channels.find("name", logs[message.guild.id].channel)
+    if(channel) {
+    var embed = new Discord.RichEmbed()
+    .setTitle(cc.guild.name)
+    .setDescription(`***Channel Created Name : *** **${cc.name}** ⬅️`)
+    .setColor(`RANDOM`)
+    .setTimestamp();
+    channel.sendEmbed(embed)
+    } // BY ! - NourEldien.#8007
+    });
    
+     client.on("deleteChannel",  dc => {
+    const channel = dc.guild.channels.find("name", logs[message.guild.id].channel)
+    if(channel) {
+    var embed = new Discord.RichEmbed()
+    .setTitle(dc.guild.name)
+    .setDescription(`***Channel Deleted Name : *** **${dc.name}** ⬅️`)
+    .setColor(`RANDOM`)
+    .setTimestamp();
+    channel.sendEmbed(embed)
+    } // BY ! - NourEldien.#8007
+    });
+   
+   
+   
+    client.on('messageUpdate', (message, newMessage) => {
+      if (message.content === newMessage.content) return;
+      if (!message || !message.id || !message.content || !message.guild || message.author.bot) return;
+      const channel = message.guild.channels.find('name', logs[message.guild.id].channel);
+      if (!channel) return;
+   
+      let embed = new Discord.RichEmbed()
+         .setAuthor(`${message.author.tag}`, message.author.avatarURL)
+         .setColor('SILVER')
+         .setDescription(`✏ **تعديل رساله
+  ارسلها <@${message.author.id}>                                                                                                                         تم تعديلها في شات** <#${message.channel.id}>\n\nقبل التعديل:\n \`${message.cleanContent}\`\n\nبعد التعديل:\n \`${newMessage.cleanContent}\``)
+         .setTimestamp();
+       channel.send({embed:embed});
+   
+    // BY ! - NourEldien.#8007 // BY ! - NourEldien.#8007
+  });
+   
+  client.on('guildMemberAdd', member => {
+      if (!member || !member.id || !member.guild) return;
+      const guild = member.guild;
+     
+      const channel = member.guild.channels.find('name', logs[message.guild.id].channel);
+      if (!channel) return;
+      let memberavatar = member.user.avatarURL
+      const fromNow = moment(member.user.createdTimestamp).fromNow();
+      const isNew = (new Date() - member.user.createdTimestamp) < 900000 ? '🆕' : '';
+     
+      let embed = new Discord.RichEmbed()
+         .setAuthor(`${member.user.tag}`, member.user.avatarURL)
+         .setThumbnail(memberavatar)
+         .setColor('GREEN')
+         .setDescription(`📥 <@${member.user.id}> **Joined To The Server**\n\n`)
+         .setTimestamp();
+       channel.send({embed:embed});
+  });
+   
+  client.on('guildMemberRemove', member => {
+      if (!member || !member.id || !member.guild) return;
+      const guild = member.guild;
+     
+      const channel = member.guild.channels.find('name', logs[message.guild.id].channel);
+      if (!channel) return;
+      let memberavatar = member.user.avatarURL
+      const fromNow = moment(member.joinedTimestamp).fromNow();
+     
+      let embed = new Discord.RichEmbed()
+         .setAuthor(`${member.user.tag}`, member.user.avatarURL)
+         .setThumbnail(memberavatar)
+         .setColor('RED')
+         .setDescription(`📤 <@${member.user.id}> **Leave From Server**\n\n`)
+         .setTimestamp();
+       channel.send({embed:embed});
+  });
+   
+  client.on('messageDelete', message => {
+      if (!message || !message.id || !message.content || !message.guild || message.author.bot) return;
+      const channel = message.guild.channels.find('name', logs[message.guild.id].channel);
+      if (!channel) return;
+     
+      let embed = new Discord.RichEmbed()
+         .setAuthor(`${message.author.tag}`, message.author.avatarURL)
+         .setColor('BLACK')
+         .setDescription(`🗑️ **حذف رساله**
+  **ارسلها <@${message.author.id}>                                                                                                                        تم حذفها في شات** <#${message.channel.id}>\n\n \`${message.cleanContent}\``)
+         .setTimestamp();
+       channel.send({embed:embed});
+   
+  });
+})
+client.on('message', message => {
+ 
+ 
+if(!message.guild) return; // BY ! - NourEldien.#8007 // BY ! - NourEldien.#8007
+  if(!logs[message.guild.id]) logs[message.guild.id] = {
+  onoff: 'Off',
+  channel: 'logs'
+  };
+ 
+if(message.content.startsWith(prefix + 'setlogs')) {
+         
+  let perm = message.member.hasPermission(`ADMINISTRATOR`) || message.member.hasPermission(`MANAGE_SERVER`)
+ 
+  if(!perm) return message.reply(`You don't have \`Manage_roles / Administrator\` Permission`);
+  let args = message.content.split(" ").slice(1);
+  if(!args.join(" ")) return message.reply(`:gear: **| Correct usage**:
+\`=setlogs toggle / set <channel name>\``);
+  let state = args[0];
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+ // BY ! - NourEldien.#8007 // BY ! - NourEldien.#8007 // BY ! - NourEldien.#8007
+  if(!state.trim().toLowerCase() == 'toggle') return message.reply(`Please type a right state ON / OFF`) ;
+    if(state.trim().toLowerCase() == 'toggle') {
+     if(logs[message.guild.id].onoff === 'Off') return [message.channel.send(`:white_check_mark: **| Logs for this server has been turned on.**`), logs[message.guild.id].onoff = 'On'];
+     if(logs[message.guild.id].onoff === 'On') return [message.channel.send(`:white_check_mark: **| Logs for this server has been turned off.**`), logs[message.guild.id].onoff = 'Off'];
+    }
+ 
+   if(state.trim().toLowerCase() == 'set') {
+   let newChannel = message.content.split(" ").slice(2).join(" ");
+   if(!newChannel) return message.reply(`:gear: **| To set the logging channel use**:
+\`=setlogs set <channel name>\``);
+     if(!message.guild.channels.find(`name`,newChannel)) return message.reply(`:mag_right: **| I can't find this channel.**`);
+    logs[message.guild.id].role = newChannel;
+     message.channel.send(`:shield: **| Logging channel has been changed to**:
+\`${newChannel}\``);
+   }
+         }
+    fs.writeFile("./logs.json", JSON.stringify(logs), (err) => {
+    if (err) console.error(err);
+  });
+}); // BY ! - NourEldien.#8007 // BY ! - NourEldien.#8007
+
+
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
+
